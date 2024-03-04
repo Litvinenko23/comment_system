6d7c7bbf4524ed1c2075fa5ae9130b583ccdc6cb
Andrey <andreylitvinenko20102010@gmail.com>
2024-03-04 14:12:40 +0200

    add Comment model

--- comments_app/models.py
+++ comments_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    message = models.TextField()
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+    def __str__(self):
+        return self.message
